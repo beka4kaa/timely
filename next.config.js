83c062ca4469b96b2aaf63263b8199fcf6cb9b5c
beka4kaa <pro100bekzhan@gmail.com>
2026-01-02 23:56:40 +0600

    Configure API Proxy via NEXT_PUBLIC_API_URL

--- next.config.js
+++ next.config.js
@@ -25,6 +25,15 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: false,
   },
+  async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${apiUrl}/api/:path*`,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
